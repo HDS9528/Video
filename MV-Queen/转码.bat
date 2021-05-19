968caaac815c00b286077c97602495662f9d87d5
web <2487378907@qq.com>
2021-05-19 21:17:17 +0800

    20210519211653

--- MV-Queen/转码.bat
+++ MV-Queen/转码.bat
@@ -4,6 +4,6 @@ rem 开始转码…
 
 rem 正在转码…
 
-ffmpeg -i 每次当我沾染了黑暗的色调.mkv -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 -hls_time 20 output/playlist.m3u8
+ffmpeg -i 抖音合集.mkv -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 -hls_time 300 output/playlist.m3u8
 
 pause
