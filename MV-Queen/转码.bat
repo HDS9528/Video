65aee4f1553369f312f95663247f0e8b9456ee8d
web <2487378907@qq.com>
2021-05-20 09:06:53 +0800

    20210520 90652

--- MV-Queen/转码.bat
+++ MV-Queen/转码.bat
@@ -4,6 +4,6 @@ rem 开始转码…
 
 rem 正在转码…
 
-ffmpeg -i 踏山河.mkv -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 -hls_time 30 output/playlist.m3u8
+ffmpeg -i 踏山河.mkv -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 -hls_time 3 output/playlist.m3u8
 
 pause
